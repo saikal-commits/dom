0e0ec4048910478a94eb9b5ed4888f0c594a8ef9
unknown <sajkal949@gmail.com>
2024-03-23 12:57:17 +0300

    iuyuiuyu

--- user.js
+++ user.js
@@ -6,12 +6,14 @@ block.style.display = "flex";
 block.style.flexDirection = "column";
 block.style.alignItems = "center";
 block.style.justifyContent = "center";
-block.style.marginLeft = "450px";
+block.style.marginLeft = "550px";
 block.style.marginTop = "80px";
 block.style.gap = "20px";
 block.style.borderRadius = "30px";
+
 const img = document.querySelector("img");
 img.style.width = "120px";
+
 const input = document.querySelectorAll("input");
 input[0].style.width = "200px";
 input[0].style.height = "25px";
@@ -21,6 +23,7 @@ input[1].style.width = "150px";
 input[1].style.height = "25px";
 input[1].style.borderRadius = "10px";
 input[1].style.border = "none";
+
 const button = document.querySelector("button");
 button.style.width = "80%";
 button.style.height = "50px";
@@ -30,4 +33,5 @@ button.style.backgroundColor = "orange";
 button.style.color = "white";
 button.style.fontSize = "20px";
 const h1 = document.querySelector("h1");
+
 h1.style.fontFamily = "sans-serif";
